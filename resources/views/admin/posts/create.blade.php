e49f05c74fad8da2e89cc9ae9b2817a65d23534f
Riccardo Pasini <riccardo.pasini1992@gmail.com>
2020-10-21 16:13:54 +0200

    autenticazione

---
+++ resources/views/admin/posts/create.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+@section('content')
+
+    <div class="container">
+        <form action="{{route('posts.store')}}" mathod="post">
+            @csrf
+            @method('POST')
+            <div class="form-group">
+                <label for="title">Titolo</label>
+                <input type="text" name="title" class="form-control" placeholder="Inserisci il titolo">
+            </div>
+            <div class="form-group">
+                <label for="body">Body</label>
+                <textarea name="body" class="form-control" rows="3" placeholder="Inserissci il testo"></textarea>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+
+
+@endsection
